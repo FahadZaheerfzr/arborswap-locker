bb1853f3fda261fea7bdfc0a39c007b263ecf726
FahadZaheerfzr <fahadzaheer19@gmail.com>
2022-12-19 18:01:20 +0500

    Connection component completed

--- src/components/Common/ConnectionModal.js
+++ src/components/Common/ConnectionModal.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { wallets } from '../../data/wallets'
+import { ConnectionContext } from '../../context/ConnectionContext/GlobalProvider';
 
 
 export default function ConnectionModal({ showModal }) {
+    const { setUpConnection } = useContext(ConnectionContext);
+
+    const handleConnection = () => {
+        setUpConnection();
+        showModal(false);
+    }
+
+
     return (
         <div className='p-9 w-[90%] max-w-[520px] bg-white rounded-[10px]'>
             <div className="flex justify-between items-center  ">
@@ -16,30 +25,32 @@ export default function ConnectionModal({ showModal }) {
                 </div>
             </div>
 
-            <div className="mt-14 grid grid-cols-2 md:grid-cols-3 gap-10">
+            <div className="mt-14 grid grid-cols-2 md:grid-cols-3 gap-8"
+                onDoubleClick={handleConnection}>
                 {wallets.map((wallet) => (
-                    <div className="flex flex-col items-center justify-center cursor-pointer" key={wallet.id}>
+                    <button className="flex focus:outline outline-primary-green py-4 px-4 flex-col items-center justify-center" key={wallet.id}>
                         <img src={wallet.image} alt={wallet.name} className="w-[50px] h-[50px] mb-4" />
                         <span className="font-gilroy font-semibold text-dark-text dark:text-light-text">{wallet.name}</span>
-                    </div>
+                    </button>
                 ))}
                 {wallets.map((wallet) => (
-                    <div className="flex flex-col items-center justify-center cursor-pointer" key={wallet.id}>
+                    <button className="flex flex-col items-center focus:outline outline-primary-green py-4 px-4 justify-center cursor-pointer" key={wallet.id}
+                        onDoubleClick={handleConnection}>
                         <img src={wallet.image} alt={wallet.name} className="w-[50px] h-[50px] mb-4" />
                         <span className="font-gilroy font-semibold text-dark-text dark:text-light-text">{wallet.name}</span>
-                    </div>
+                    </button>
                 ))}
             </div>
-            
+
             <div className='flex justify-center mt-12'>
                 <span className='text-gray dark:text-gray-dark font-semibold font-gilroy'>
-                Haven’t got a crypto wallet yet?
+                    Haven’t got a crypto wallet yet?
                 </span>
             </div>
-            
+
             <div className='flex justify-center mt-5'>
                 <span className='text-primary-green font-semibold font-gilroy'>
-                Learn how to connect
+                    Learn how to connect
                 </span>
             </div>
         </div>
