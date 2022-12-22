f82828dc492c32fd7df41f82d8d4f6c28f5697c0
WongJapan <kulomilyarder@gmail.com>
2022-12-23 01:36:17 +0700

    checkpoint

--- src/components/Locker/CardView/Card.js
+++ src/components/Locker/CardView/Card.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import CardInfo from './CardInfo'
 import Timer from './Timer'
 
-export default function Card({ id, name1, name2, icon1, icon2, amount, amountUSD, unlock_date, token }) {
+export default function Card({ data, token = false }) {
   return (
     <div className="rounded-[20px] bg-white dark:bg-dark-1">
       <div className="px-6">
         <div className="flex justify-between items-center border-b border-dim-text dark:border-dim-text-dark border-dashed border-opacity-30 mt-3 py-5">
           <div className="flex items-center">
             <div className="flex items-center">
-              <img className="w-9 h-9" src={icon1} alt={name1} />
-              {!token && <img className="relative -left-5 w-9 h-9" src={icon2} alt={name2} />}
+              {token && <img className="w-9 h-9" src={data.info.logoImage} alt="BLANK" />}
             </div>
 
             <div
@@ -20,13 +19,13 @@ export default function Card({ id, name1, name2, icon1, icon2, amount, amountUSD
               }`}
             >
               <span>
-                {name1}
-                {!token && `/${name2}`}
+                {data.address}
+                {!token && `/${data.address}`}
               </span>
               <span className="text-xs font-medium text-dim-text dark:text-dim-text-dark">{token}</span>
             </div>
           </div>
-          <Link to={`locked-assets/${id}`}>
+          <Link to={`locked-assets/${data.address}`}>
             <div className="flex items-center">
               <span className="flex items-center font-medium text-sm font-gilroy text-primary-green ">View</span>
               <img className="rotate-180" src="/images/sidebar/arrow-left.svg" alt="arrow-right" />
@@ -35,9 +34,9 @@ export default function Card({ id, name1, name2, icon1, icon2, amount, amountUSD
         </div>
 
         <div className="flex flex-col justify-between">
-          <CardInfo heading={'Amount'} value={amount.toLocaleString()} />
-          <CardInfo heading={'Amount ($)'} value={amountUSD.toLocaleString()} />
-          <CardInfo heading={'Unlock date'} value={unlock_date} />
+          <CardInfo heading={'Amount'} value={data.info.amount.toString().toLocaleString()} />
+          <CardInfo heading={'Amount ($)'} value={0} />
+          <CardInfo heading={'Unlock date'} value={data.info.unlockDate.toNumber()} />
         </div>
       </div>
 
@@ -45,7 +44,7 @@ export default function Card({ id, name1, name2, icon1, icon2, amount, amountUSD
         <div className="flex justify-between items-center">
           <span className="font-medium text-xs text-gray dark:text-gray-dark">Unlocks In</span>
 
-          <Timer date={unlock_date} />
+          <Timer date={data.info.unlockDate.toNumber() * 1000} />
         </div>
       </div>
     </div>
