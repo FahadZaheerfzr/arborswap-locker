219979e24b18c0bf7bac42a5d4fef0005e3a5de6
WongJapan <kulomilyarder@gmail.com>
2022-12-23 01:36:07 +0700

    checkpoint

--- src/components/Locker/index.js
+++ src/components/Locker/index.js
@@ -1,39 +1,16 @@
-import React, { useEffect } from 'react'
-import { getLockList } from 'utils/getLockList'
+import React from 'react'
 import { Cards, Tokens } from '../../data/cards'
 import { useDocumentTitle } from '../../hooks/setDocumentTitle'
 import Card from './CardView/Card'
 import List from './ListView/List'
 
-export default function LockerBase({ cardFormat, itemSelected }) {
+export default function LockerBase({ cardFormat, itemSelected, tokenList, liquidityList }) {
   useDocumentTitle('Locker')
-
-  const handleFetch = async () => {
-    const data = await getLockList()
-    console.log(data)
-    return data
-  }
-
-  useEffect(() => {
-    handleFetch()
-  }, [])
 
   return cardFormat === 'grid' ? (
     <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-5 mt-7">
       {itemSelected === 'liquidity' &&
-        Cards.map((card) => (
-          <Card
-            key={card.id}
-            id={card.id}
-            name1={card.name1}
-            name2={card.name2}
-            icon1={card.icon1}
-            icon2={card.icon2}
-            amount={card.amount}
-            amountUSD={card.amountUSD}
-            unlock_date={card.unlock_date}
-          />
-        ))}
+        liquidityList.map((liquidity, index) => <Card key={index} data={liquidity} token={false} />)}
 
       {itemSelected === 'token' &&
         Tokens.map((card) => (
