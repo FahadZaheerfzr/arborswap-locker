37306eeb8df34d23e0a1b3a95625617cc6cb30b4
FahadZaheerfzr <fahadzaheer19@gmail.com>
2022-12-23 16:46:17 +0500

    Dropdown topbar added

---
+++ src/components/Topbar/Dropdown.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import DisconnectSVG from 'svgs/Topbar/Disconnect'
+
+export default function Dropdown() {
+  return (
+    <div className='absolute w-[250px] mt-14 rounded-md border border-[#e7e3ff] dark:border-dim-text-dark bg-white dark:bg-dark-1'>
+        <div className='flex items-center justify-between border border-gray dark:border-gray-dark border-opacity-20 px-4 py-4'>
+          <span className='font-medium text-gray dark:text-gray-dark'>
+            Wallet
+          </span>
+
+          <img src="/images/topbar/warning.svg" alt="warning" />
+        </div>
+        
+        <div className='flex items-center justify-between border border-gray dark:border-gray-dark border-opacity-20 px-4 py-4'>
+          <span className='font-medium text-gray dark:text-gray-dark'>
+            Transactions
+          </span>
+        </div>
+        <div className='flex items-center justify-between border border-gray dark:border-gray-dark border-opacity-20 px-4 py-4'>
+          <span className='font-medium text-gray dark:text-gray-dark'>
+            Make a Profile
+          </span>
+
+          <div className='rounded-full w-2 h-2 bg-[#e56060]' />
+        </div>
+        <div className='flex items-center justify-between border border-gray dark:border-gray-dark border-opacity-20 px-4 py-4'>
+          <span className='font-medium text-gray dark:text-gray-dark'>
+            Disconnect
+          </span>
+
+          <DisconnectSVG className='fill-dark-text dark:fill-light-text' />
+        </div>
+    </div>
+  )
+}
